24a151a49dccbd76685406f57df30125fdbcbdd4
Matteo Barato <matteobarato95@gmail.com>
2020-04-21 15:40:32 +0200

    added option to preload video

--- simulate/simulate_camera.py
+++ simulate/simulate_camera.py
@@ -20,17 +20,40 @@ class FrameWrapper():
 
 
 class SimulateCamera(BaseSimulation):
-    def __init__(self, video_path, info_path, freq=0.2, speed=1, verbose=1, ):
+    def __init__(self, video_path, info_path, freq=0.2, speed=1, verbose=1, preload=True):
         super().__init__(freq=freq, speed=speed, verbose=verbose)
         self.video_path = video_path
         self.info_path = info_path
+        self.preload = preload
         self.df_info = pd.read_csv(self.info_path, names=[
                                 'timestamp', 'frame_index'])
+        if (self.preload):
+            self.cached_frames = []
+            self.preload_video()
+
 
     def next_timestamp(self, index):
         if len(self.df_info['frame_index']) <= index:
             return None
         return self.df_info['timestamp'][index]
+
+    def preload_video(self):
+        if(self.verbose):
+            print("Loading video to cache")
+        cap = cv2.VideoCapture(self.video_path)
+        frame_index = 0
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret == True:
+                timestamp = self.next_timestamp(frame_index)
+                frame_wrapped = FrameWrapper(frame, timestamp)
+                self.cached_frames.append(frame_wrapped)
+                frame_index += 1
+            else:
+                break
+        # Release everything if job is finished
+        cap.release()
+        cv2.destroyAllWindows()
 
     def read_video(self, callback):
         cap = cv2.VideoCapture(self.video_path)
@@ -58,12 +81,25 @@ class SimulateCamera(BaseSimulation):
         # Release everything if job is finished
         cap.release()
         cv2.destroyAllWindows()
+    
+    def read_cache(self, callback):
+        for frame in self.cached_frames:
+            if self.verbose:
+                print(f"frame {frame.timestamp} red...")
+            callback(frame)
+            time.sleep(self._freq)
+        if self.verbose:
+                    print("no frame read")
 
     def run(self, callback):
         if self.verbose:
             print("preparing t1...")
-        self.thread = threading.Thread(
-            name="reader", target=self.read_video, args=(callback,))
+        if self.preload:
+            self.thread = threading.Thread(
+                name="reader", target=self.read_cache, args=(callback,))
+        else:
+             self.thread = threading.Thread(
+                name="reader", target=self.read_video, args=(callback,))
         self.thread.start()
         if self.verbose:
             print("reader started...")
